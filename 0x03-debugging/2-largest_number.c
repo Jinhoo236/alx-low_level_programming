c3604fea3715c7bacdf49802a7a2b576c5a8002d
Omojaro Oluwaseyi <omojarooluwaseyi@gmail.com>
2022-09-15 12:47:18 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,7 +13,9 @@ int largest_number(int a, int b, int c)
 	int largest;
 	if (a >= b && a >= c)
 	{
+
 		largest = a;
+
 }
 	else if (b >= a && b >= c)
 	{
@@ -23,6 +25,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 }
+
 	return (largest);
 
 }
