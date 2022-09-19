cd5efb7f5d9ae876a456695427eeb68b2de08aa1
Omojaro Oluwaseyi <omojarooluwaseyi@gmail.com>
2022-09-19 14:00:38 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_strings - reverse the strings 
+ * @s: string to reverse
+ */
+
+void rev_string(char *s)
+{
+	int i, len = strlen(s), j = 0;
+	char temp;
+
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		j++;
+	}
+}
+
