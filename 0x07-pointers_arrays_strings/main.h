49c4b41fb479a455b3f64e5d11286c7979c5ab53
Omojaro Oluwaseyi <omojarooluwaseyi@gmail.com>
2022-09-27 09:24:15 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,15 +2,15 @@
 #define MAIN_H
 
 /**
- * Void prototype()
- *int prototype()
-/
+ * void prototypes()
+ * int prototypes()
+ */
 
-int _putchar(char)
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsighned int n);
-char * strchr(char *s, char c);
-unsigned int _strspn (char *s, char *accept);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
